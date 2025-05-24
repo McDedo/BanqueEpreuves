e06c7b2a7c3f72ce000f7c7fbcbd46ca0c96eb00
McDedo <dedocaleb1@gmail.com>
2025-05-24 22:47:14 +0100

    Fix ALLOWED_HOSTS for Render

--- banque_epreuves/settings.py
+++ banque_epreuves/settings.py
@@ -30,7 +30,6 @@ DEBUG = False
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = ['banqueepreuves.onrender.com']
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
 
 
 # Application definition
