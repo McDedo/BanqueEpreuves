c1d3c609a6e9ae4adae2f37d233139aef6411712
McDedo <dedocaleb1@gmail.com>
2025-05-25 02:06:22 +0100

    File settings update

--- banque_epreuves/settings.py
+++ banque_epreuves/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "clé-par-défaut-invalide")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-DEBUG = os.environ.get("DEBUG", "False") == "True"
+DEBUG = os.environ.get("DEBUG", "False")="True"
 
 
 ALLOWED_HOSTS = ['banqueepreuves.onrender.com', '127.0.0.1', 'localhost']
