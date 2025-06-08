381a2ed6037789bd2664b04d4ed359f47d677827
McDedo <dedocaleb1@gmail.com>
2025-06-08 06:05:41 +0100

    New migrations

---
+++ blog/migrations/0008_populate_matieres.py
@@ -0,0 +1,33 @@
+from django.db import migrations
+
+def add_matieres(apps, schema_editor):
+    Matiere = apps.get_model('blog', 'Matière')
+    matieres = [
+        "Mathématiques",
+        "Physique-Chimie",
+        "Français",
+        "SVT",
+        "Histoire-Géographie",
+        "Philosophie",
+        "EST",
+        "Ar",
+        "EPS",
+        "Informatique",
+        "Économie",
+        "Technologie",
+        "Anglais",
+        "Espagnol",
+        "Allemand"
+    ]
+    for nom in matieres:
+        Matiere.objects.get_or_create(nom=nom)
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('blog', '0007_auto_20250608_0438'),  
+    ]
+
+    operations = [
+        migrations.RunPython(add_matieres),
+    ]
