b41ce7b4303412fc40cdbd6b0bae798653d4b5e5
McDedo <dedocaleb1@gmail.com>
2025-06-26 04:58:53 +0100

    ghost

--- banque_epreuves/wsgi.py
+++ banque_epreuves/wsgi.py
@@ -13,4 +13,9 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'banque_epreuves.settings')
 
+
+print("🔧 DJANGO_SETTINGS_MODULE:", os.getenv("DJANGO_SETTINGS_MODULE"))
+print("🔧 SECRET_KEY:", os.getenv("SECRET_KEY"))
+
+
 application = get_wsgi_application()
